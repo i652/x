6e440b4906bf32ce2ba205333ed8f89c2e4d9b18
Egor <cqk@mail.ru>
2020-07-05 16:50:04 +0300

    cycles

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 const personalMoviesDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: [],
+    genres: {},
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+if (personalMoviesDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} 
+else if (personalMoviesDB.count > 10 && personalMoviesDB.count <= 30) {
+    alert('Вы классический зритель');
+}
+else if (personalMoviesDB.count > 30) {
+    alert('Вы киноман');
+} 
+else {
+    alert('Произошла ошибка');
+}
 
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?','');
+
+if (a != null && b != null && a != '' && b != '' && a.length <50) {
+    personalMoviesDB.movies[a] = b;
+    alert('done');
+} else {
+    alert('error!');
+    i--;
+}
+}
 
 console.log(personalMoviesDB);
