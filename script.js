87489885c82c13c97a83578e5900702fbc57a3fd
Egor <cqk@mail.ru>
2020-07-05 12:34:41 +0300

    movies script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Привет, Мир!');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB);
